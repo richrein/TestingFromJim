24cf54329968546f5eb227ee7817f9de822c4038
Jim Hatcher <jimhatcher@jhatcher-rmbp15.local>
2017-03-30 15:53:31 -0500

    changed query pattern to use 13 digit values and mutliple ngrams

--- solrqueryngramcaptureaveragetime/src/main/java/com/fedextesting/SolrQueryNgramCaptureAverageTime.java
+++ solrqueryngramcaptureaveragetime/src/main/java/com/fedextesting/SolrQueryNgramCaptureAverageTime.java
@@ -14,16 +14,24 @@ public class SolrQueryNgramCaptureAverageTime {
     private static Session m_session;
     private static String m_clusterAddress = "";
     private static String m_keyspaceName = "";
+    private static Integer m_numberOfQueries = 0;
 
     public static void main(String[] args){
 
-        if (args.length != 2){
-            System.out.println("You must pass in the following arguments to this program: cluster_address keyspace_name");
-            System.out.println("For example: 54.201.21.89 x");
+        if (args.length != 3){
+            System.out.println("You must pass in the following arguments to this program: cluster_address keyspace_name number_of_queries_to_run");
+            System.out.println("For example: 54.201.21.89 x 1000");
             return;
         } else {
             m_clusterAddress = args[0];
             m_keyspaceName = args[1];
+            try{
+                m_numberOfQueries = new Integer(args[2]);
+            }
+            catch(Exception e){
+                System.out.println("Invalid value passed as number_of_queries_to_run.  Defaulting to 1,000");
+                m_numberOfQueries = 1000;
+            }
         }
 
         //set default values if not passed in as arguments
@@ -32,6 +40,9 @@ public class SolrQueryNgramCaptureAverageTime {
         }
         if (m_keyspaceName == ""){
             m_keyspaceName = "x";
+        }
+        if (m_numberOfQueries == 0){
+            m_numberOfQueries = 1000;
         }
 
         InitializeClusterAndSession();
@@ -43,19 +54,21 @@ public class SolrQueryNgramCaptureAverageTime {
         //String solrQuery = "select * from " + m_keyspaceName + ".y where solr_query = 'crid: \"?\" AND crid_ngram: \"?\"';";
         //PreparedStatement preparedStatement = m_session.prepare(solrQuery);
 
-        for(int i = 0; i <= 100; i++){
+        System.out.println("Executing " + m_numberOfQueries.toString() + " queries");
+        System.out.println();
+
+        for(int i = 0; i <= m_numberOfQueries; i++){
             //get a value from Cassandra
             String wholeTerm = getRandomTerm();
-            String shortenedTerm = "";
-            Integer shortendedSize = 5;
-            if (wholeTerm.length() > shortendedSize){
-                shortenedTerm = wholeTerm.substring(0, shortendedSize);
-            } else {
-                shortenedTerm = wholeTerm;
-            }
+            //the whole term should always be 15 digits
+            Integer shortendedSize = 13;
+            String shortenedTerm = wholeTerm.substring(0, shortendedSize);
+            String firstFive = wholeTerm.substring(0, 5);
+            String secondFive = wholeTerm.substring(5, 10);
+            String thirdThree = wholeTerm.substring(10, 13);
 
             //put together Solr Query
-            String solrQuery = "select crid, addr1 from " + m_keyspaceName + ".y where solr_query = 'crid: " + wholeTerm + "* AND crid_ngram: " + shortenedTerm + "' LIMIT 1;";
+            String solrQuery = "select crid, addr1 from " + m_keyspaceName + ".y where solr_query = '{ \"q\":\"crid:*.*\", \"fq\":[ \"{!cached=false cost=1}crid_ngram: " + firstFive + "\", \"{!cached=false cost=1}crid_ngram: " + secondFive + "\", \"{!cached=false cost=1}crid_ngram: " + thirdThree + "\", \"{!cached=false cost=101}crid: " + shortenedTerm + "*\" ] }' LIMIT 1;";
             System.out.println(solrQuery);
 
             //run query
@@ -116,14 +129,15 @@ public class SolrQueryNgramCaptureAverageTime {
         ResultSet results = m_session.execute(cql);
         String crid = results.one().getString("crid");
 
-        int lowerLimit = 7;
-        int upperLimit = 10;
-        int randomLength = random.nextInt(upperLimit - lowerLimit) + lowerLimit;
-        if (crid.length() < randomLength) {
-            return crid;
-        } else {
-            return crid.substring(0, randomLength);
-        }
+//        int lowerLimit = 7;
+//        int upperLimit = 10;
+//        int randomLength = random.nextInt(upperLimit - lowerLimit) + lowerLimit;
+//        if (crid.length() < randomLength) {
+//            return crid;
+//        } else {
+//            return crid.substring(0, randomLength);
+//        }
+        return crid;
     }
 
 }
